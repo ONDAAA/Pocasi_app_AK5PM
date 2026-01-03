44ee8ef5c18ee370bc943ebb29b9e2dcd95d7a20
swpv <swpv@email.cz>
2026-01-03 01:53:51 +0100

    feat: generate app pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,20 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'search',
+    loadComponent: () => import('./pages/search/search.page').then( m => m.SearchPage)
+  },
+  {
+    path: 'detail',
+    loadComponent: () => import('./pages/detail/detail.page').then( m => m.DetailPage)
+  },
+  {
+    path: 'favorites',
+    loadComponent: () => import('./pages/favorites/favorites.page').then( m => m.FavoritesPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
 ];
