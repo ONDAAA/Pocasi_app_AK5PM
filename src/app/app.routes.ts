1ab0756cc8e62462b0f48a17d42ac19fa6428ff2
swpv <swpv@email.cz>
2026-01-24 21:46:24 +0100

    feat: minor fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -29,18 +29,7 @@ export const routes: Routes = [
   },
 
 
-  {
-    path: 'search',
-    loadComponent: () => import('./pages/search/search.page').then(m => m.SearchPage),
-  },
-  {
-    path: 'detail',
-    loadComponent: () => import('./pages/detail/detail.page').then(m => m.DetailPage),
-  },
-  {
-    path: 'favorites',
-    loadComponent: () => import('./pages/favorites/favorites.page').then(m => m.FavoritesPage),
-  },
+
   {
     path: 'settings',
     loadComponent: () => import('./pages/settings/settings.page').then(m => m.SettingsPage),
